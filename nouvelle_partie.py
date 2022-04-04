6cdb596d945b6db49019eb12cb187036bb891e71
Unknwn <96542420+Malti789@users.noreply.github.com>
2022-04-04 15:20:24 +0200

    Update nouvelle_partie.py

--- nouvelle_partie.py
+++ nouvelle_partie.py
@@ -10,7 +10,6 @@ import tkinter
 window = Tk()                       #Créer une fenêtre 
 window.title("Lighthouse")          #Titre de la fenêtre
 window.geometry("1080x720")         #Taille de la fenêtre
-""" window.iconbitmap("lighthouse.ico") #Icône de la fenêtre """
 window.config(background="black")   #Couleur de fond de la fenêtre
 
 """FONCTIONS"""
@@ -21,5 +20,19 @@ def new():
     player_name.pack()
 
 """CONTENU DE LA FENETRE"""
+
+bouton_nouvelle_partie= Button(window, text="Nouvelle Partie")
+bouton_nouvelle_partie.pack()
+
+bouton_charger_partie= Button(window, text="Charger Partie")
+bouton_charger_partie.pack()
+
+bouton_credits= Button(window, text="Crédits")
+bouton_credits.pack()
+
+bouton_quitter= Button(window, text="Quitter", command=window.quit)
+bouton_quitter.pack()
+
+""""""
 
 window.mainloop()
